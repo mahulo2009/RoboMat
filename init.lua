29a428953b1339ff03adf32233f00d6b27f6616f
manuel <manuel.huertas@gtc.iac.es>
2017-02-08 13:53:44 +0000

    Initial commit, connection as access point

---
+++ init.lua
@@ -0,0 +1,13 @@
+cfg={}
+cfg.ssid="ESP8266";
+cfg.pwd="12345678";
+wifi.ap.config(cfg);
+
+cfg={}
+cfg.ip="192.168.1.1";
+cfg.netmask="255.255.255.0";
+cfg.gateway="192.168.1.1";
+wifi.ap.setip(cfg);
+wifi.setmode(wifi.SOFTAP)
+
+collectgarbage();
