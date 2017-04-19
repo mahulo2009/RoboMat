fd965b0f6a3017337106243aeee06f4b5b6fb6c0
Manuel <manuel.huertas.lopez@gmail.com>
2017-04-19 19:27:55 +0100

    velocity calculation

--- arduino/robot_mat/robot_mat.ino
+++ arduino/robot_mat/robot_mat.ino
@@ -5,9 +5,18 @@
 #include <rosserial_arduino/MotorMove.h>
 #include <rosserial_arduino/EngineMove.h>
 #include <rosserial_arduino/EngineEncoder.h>
+#include "motor.h"
 
 const char* ssid = "***";
-const char* password = "***";
+const char* password = "***";  
+
+Motor *engine[2];
+
+
+long previous_time=0;
+
+int previous_pos_1=0;
+int previous_pos_2=0;
 
 IPAddress server(192, 168, 1, 40); // ip of your ROS server
 IPAddress ip_address;
@@ -41,51 +50,31 @@ public:
 };
 
 ros::NodeHandle_<WiFiHardware>  nh;
-
-int wheel_1_power = 5;        
-int wheel_1_direction = 0;           
-int wheel_1_position = 14;
-   
-int wheel_2_power = 4;           
-int wheel_2_direction = 2;          
-int wheel_2_position = 12;
-
+/**
+ * 
+ */
 void messageMotorMove( const rosserial_arduino::MotorMove& msg) {  
   nh.logdebug("Motor Move");
-
-  int duty = floor((1023/100) * msg.power);
-  
-  if ( msg.number == 1 ) {
-    analogWrite(wheel_1_power,duty);
-    if( msg.direction== 0 ) {
-      digitalWrite(wheel_1_direction,LOW);
-    } else if( msg.direction== 1 ) {
-      digitalWrite(wheel_1_direction,HIGH);
-    }    
-  } else if (msg.number == 2 ) {
-    analogWrite(wheel_2_power,duty);
-    if( msg.direction== 0 ) {
-      digitalWrite(wheel_2_direction,LOW);
-    } else if( msg.direction== 1 ) {
-      digitalWrite(wheel_2_direction,HIGH);
-    }
-  }
+  if ( (msg.number <0) || (msg.number >1) ) {
+    nh.logdebug("Number motor out of range");
+    return;
+  }  
+  int result = engine[msg.number]->move(msg.power,msg.direction);
+  if (result == -1) {
+    nh.logdebug("Out of range");
+  }  
 }
 ros::Subscriber<rosserial_arduino::MotorMove> motor_move("motor_move", &messageMotorMove);
-
+/**
+ * 
+ */
 void messageEngineMove( const rosserial_arduino::EngineMove& msg) {  
-  nh.logdebug("Engine Move");
-
-  int duty = floor((1023/100) * msg.power);
-  
-  analogWrite(wheel_1_power,duty);
-  analogWrite(wheel_2_power,duty);
-  if( msg.direction== 0 ) {
-    digitalWrite(wheel_1_direction,LOW);
-    digitalWrite(wheel_2_direction,LOW);
-  } else if( msg.direction== 1 ) {
-    digitalWrite(wheel_1_direction,HIGH);
-    digitalWrite(wheel_2_direction,HIGH);
+  nh.logdebug("Engine Move");  
+  for (int i=0;i<2;i++) {
+    int result = engine[i]->move(msg.power,msg.direction);
+      if (result == -1) {
+        nh.logdebug("Out of range");
+      }
   }
 }
 ros::Subscriber<rosserial_arduino::EngineMove> engine_move("engine_move", &messageEngineMove);
@@ -94,18 +83,19 @@ rosserial_arduino::EngineEncoder engine_encoder;
 ros::Publisher motor_encoder("motor_encoder", &engine_encoder);
 
 void setup() {
-  pinMode(wheel_1_power, OUTPUT);
-  pinMode(wheel_1_direction, OUTPUT);
-  pinMode(wheel_1_position, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(wheel_1_position), update_wheel_1_position, RISING);  
-  pinMode(wheel_2_power, OUTPUT);
-  pinMode(wheel_2_direction, OUTPUT);  
-  pinMode(wheel_2_position, INPUT_PULLUP);  
-  attachInterrupt(digitalPinToInterrupt(wheel_2_position), update_wheel_2_position, RISING);
 
   Serial.begin(115200);
   setupWiFi();
   delay(2000);
+  
+  engine[0] = new Motor(5,0,14);
+  engine[1] = new Motor(4,2,12);
+  for (int i=0;i<2;i++) {
+    engine[i]->setup();
+  }  
+  attachInterrupt(digitalPinToInterrupt(14), update_wheel_1_position, RISING);        
+  attachInterrupt(digitalPinToInterrupt(12), update_wheel_2_position, RISING);        
+
   
   nh.initNode();
   nh.subscribe(motor_move);
@@ -114,24 +104,37 @@ void setup() {
 
   engine_encoder.encoder_1=0;
   engine_encoder.encoder_2=0;  
+
+  previous_time=millis();
 }
 
 void loop() {
-  nh.spinOnce();
+  if (millis() > previous_time + 1000 ) {    
+    engine_encoder.velocity_1=engine[0]->getPosition()-previous_pos_1;
+    engine_encoder.velocity_2=engine[1]->getPosition()-previous_pos_2;  
+
+    previous_pos_1=engine[0]->getPosition();
+    previous_pos_2=engine[1]->getPosition();    
+    previous_time=millis();
+  }
+
+  engine_encoder.encoder_1=engine[0]->getPosition();
+  engine_encoder.encoder_2=engine[1]->getPosition();  
+
   motor_encoder.publish(&engine_encoder);
-  delay(30); 
+  delay(100);
+  nh.spinOnce();
 }
 
-void update_wheel_1_position() {
-     engine_encoder.encoder_1=engine_encoder.encoder_1+1;
+void update_wheel_1_position() { 
+  engine[0]->updatePosition();
 }
 
 void update_wheel_2_position() {
-    engine_encoder.encoder_2=engine_encoder.encoder_2+1;
+  engine[1]->updatePosition();    
 }
 
-void setupWiFi()
-{
+void setupWiFi() {
   WiFi.begin(ssid, password);
   Serial.print("\nConnecting to "); Serial.println(ssid);
   uint8_t i = 0;
@@ -144,4 +147,3 @@ void setupWiFi()
   Serial.print(WiFi.localIP());
   Serial.println(" to access client");
 }
-
